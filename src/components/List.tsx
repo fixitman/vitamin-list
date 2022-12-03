d0dea9afc92f93556a5e125235e4d892b9426bd6
mcollins <fixitman.mike@gmail.com>
2022-12-03 02:45:47 -0500

    created listitem component

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './List.css'
 import { item } from '../data'
+import ListItem from './ListItem'
 
 
 function List(props: { list: item[] }) {
@@ -18,18 +19,12 @@ function List(props: { list: item[] }) {
         items.length === 0 && <li className='emptyItem'>Nothing to do!</li>
       }
 
-
       {
         items.length > 0 && (
-          items.map((i) => (<li key={i.id} >
-            <input id={i.id.toString()} type='checkbox' />
-            <label htmlFor={i.id.toString()}>{i.todo}</label>
-            <button
-              className='deleteButton'
-              onClick={() => deleteItem(i.id)}
-            >
-              X </button>
-          </li>)
+
+          items.map((i) => (
+          
+          <ListItem listItem={i} deleteItem={deleteItem}/>)
           )
         )
       }
---
+++ src/components/ListItem.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {item} from '../data'
+
+
+export interface ListItemProps {
+    listItem: item,
+    deleteItem: (id:number)=>void
+}
+
+
+const ListItem = (props: ListItemProps) =>{
+    let i = props.listItem
+  return (
+    <li key={i.id} >
+            <input id={i.id.toString()} type='checkbox' />
+            <label htmlFor={i.id.toString()}>{i.todo}</label>
+            <button
+              className='deleteButton'
+              onClick={() => props.deleteItem(i.id)}
+            >
+              X </button>
+          </li>
+  )
+}
+
+export default ListItem
